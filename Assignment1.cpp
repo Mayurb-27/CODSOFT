c8ca5449c04cd85613217f959441a2b8994c245f
Mayurb27 <118687618+Mayurb-27@users.noreply.github.com>
2023-08-12 14:52:02 +0530

    Add files via upload

---
+++ Assignment1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int ran_num = rand() % 30 + 1;  // Generating a random number
+    
+    int guess_num;
+    int attempt = 0;
+
+    cout << "Welcome to 'Guessing the Number Game'" << endl; 
+    cout << endl;
+
+    do {
+        cout << "Can you guess a number between 1 & 30: ";   // User enters the number
+        cin >> guess_num;
+
+        attempt = attempt + 1;   //increment attempt counter
+        
+        if (guess_num > ran_num) {
+            cout << "You are too high!!" << endl;
+        }
+        else if (guess_num < ran_num) {
+            cout << "You are too low!!" << endl;
+        } 
+        else {
+            cout << "Bravo! You correctly guessed the number in " << attempt << " attempts!!" << endl;
+        }
+        } 
+    while (guess_num != ran_num);
+    
+    return 0;
+}
---
+++ Assignment3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+
+int main() {
+    int Num_Students;
+
+    std::cout << "Enter the number of students: ";
+    std::cin >> Num_Students;
+
+    std::string Names[Num_Students];
+    int Grades[Num_Students];
+
+    for (int i = 0; i < Num_Students; ++i) {
+        std::cout << "Enter student name: ";
+        std::cin >> Names[i];
+
+        std::cout << "Enter student grade: ";
+        std::cin >> Grades[i];
+    }
+
+    int Total_Grades = 0.0;
+    for (int i = 0; i < Num_Students; ++i) {
+        Total_Grades += Grades[i];
+    }
+
+    int Average_Grade = Total_Grades / Num_Students;
+
+    int Highest_Grade = *std::max_element(Grades, Grades + Num_Students);
+    int Lowest_Grade = *std::min_element(Grades, Grades + Num_Students);
+
+    std::cout << "\nAverage grade = " << Average_Grade << std::endl;
+    std::cout << "Highest grade = " << Highest_Grade << std::endl;
+    std::cout << "Lowest grade = " << Lowest_Grade << std::endl;
+
+    return 0;
+}
